da652d8f45b52539c2e716ac39b8eb7ad97bfc17
Aya Benzidi <ayabenzidi27@gmail.com>
2023-04-18 01:51:17 +0000

    first commit

---
+++ src/main/java/ma/enset/hospitalapp/HospitalAppApplication.java
@@ -0,0 +1,33 @@
+package ma.enset.hospitalapp;
+
+import ma.enset.hospitalapp.entities.Patient;
+import ma.enset.hospitalapp.repository.PatientRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Date;
+
+@SpringBootApplication
+public class HospitalAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalAppApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner start(PatientRepository patientRepository){
+        return args -> {
+
+            patientRepository.save(new Patient(null,"Khaoula",new Date(),true,98));
+
+            patientRepository.save(new Patient(null,"Laila",new Date(),true,123));
+        };
+    }
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
